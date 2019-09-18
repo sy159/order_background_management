95c185cd9dd8ab9d388e83e3035141a344e60247
如何好听 <40220141+sy159@users.noreply.github.com>
2019-09-18 10:01:05 +0800

    Update __init__.py

--- orange_web/__init__.py
+++ orange_web/__init__.py
@@ -4,6 +4,9 @@ from django.db import connections
 pymysql.install_as_MySQLdb()
 
 
-def close_old_connections():
+def close_old_connections(**kwargs):
     for conn in connections.all():
         conn.close_if_unusable_or_obsolete()
+
+signals.request_started.connect(close_old_connections)
+signals.request_finished.connect(close_old_connections)
